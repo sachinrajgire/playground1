4bc857e20f99e69660f018f8ae3dc0c65368c76f
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-02-25 05:49:04 -0500

    d

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 
 
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css'
 import './beststyles.scss'
+import data from './Student_Data.json' ;
+
+// Career_Url: "https://ibegin.tcs.com/iBegin/jobs/search"
+// Employer: "TCS"
+// Graduation_Year: 2020
+// Job_Start_Date: "6/20/2020"
+// Job_Title: "Software Engineer"
+// Specialization: "Computer Science"
+// University_Name: "University of North Carolina at Charlotte"
+
 
 
 function App() {
 
-   
-
-
+const univName =data.map(i=>{
+const {Employer, Career_Url, Job_Title} = i 
+    return (
+    <div style={{margin:'10px',padding:'10px', backgroundColor:"lightblue"}}>
+    <div>
+        <a href={Career_Url}>Career Site </a>
+        
+        </div>
+    <div>{Employer}</div>   
+    <div>{Job_Title}</div>
+    </div>)
     
-return (
+})  
+  
+        
+        
+  
+return (  
 
-   
-<div class='container' display="mobile" isthismobiledevice='yes' >
-
-
-
-JAVASCRIPT PLAYGORUND 
-
-
+<div>
+  {univName}
+    
 </div>
 
 
-)       
+
+) ;      
 
 }
 
--- src/index.js
+++ src/index.js
@@ -15,3 +15,28 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// let str=`I can code all Night long`
+// let regex=/\s[a-z]+/gm
+
+// let ret= str.replace(regex,(arg)=>{
+
+//   console.log(arg,'arg');
+// let play= arg.charAt(1).toUpperCase() + arg.slice(2)
+// console.log(play,'play');
+// return ` ${play}`
+// })
+
+// let str=`Three girls went to forest .Monkey said yee yaaaaaw `
+// let regex=/([a-z])\1+/gmi
+
+// let ret = str.match(regex)
+// console.log(ret,'ret');
+// return ret.join("")
+
+// let str=`When boy went to boarding school, he was fine`
+// let regex=/\sb[a-z]+/gmi
+
+// let ret = str.match(regex)
+// console.log(ret,'ret');
+// return ret[0].trim().length  
