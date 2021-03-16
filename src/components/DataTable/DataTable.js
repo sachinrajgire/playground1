6dcff5f39f95d87197a0d2c96ad1188293dd68d3
Kinjal <kinjalpm96@gmail.com>
2021-03-15 22:17:39 -0400

    pluto-1- more space for Job Start in Table in Records

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -25,6 +25,7 @@ const StyledTableCell = withStyles((theme) => ({
   head: {
     backgroundColor: '#ffd51e',
     color: 'black',
+    width: '20%'
   },
   body: {
     fontSize: 14,
@@ -52,19 +53,19 @@ export default function CustomizedTables({data}) {
   const history= useHistory()
 
   return (
-    <TableContainer component={Paper}>
+    <TableContainer component={Paper} >
     <Badge style={{margin:'20px'}} color="success"> {`Found ${data.length} records`}</Badge>
       <Table className={classes.table} aria-label="customized table">
         <TableHead>
           <TableRow>
             <StyledTableCell>Employer</StyledTableCell>
-            <StyledTableCell align="right">Job Title</StyledTableCell>
-            <StyledTableCell align="right">Job Start Date</StyledTableCell>
-            <StyledTableCell align="right">University Name</StyledTableCell>
-            <StyledTableCell align="right">Graduation Date</StyledTableCell>
-            <StyledTableCell align="right">Specialization</StyledTableCell>
-            <StyledTableCell align="right">Career Link</StyledTableCell>
-            <StyledTableCell align="right">Edit</StyledTableCell>
+            <StyledTableCell align="left">Job Title</StyledTableCell>
+            <StyledTableCell align="left">Job Start Date</StyledTableCell>
+            <StyledTableCell align="left">University Name</StyledTableCell>
+            <StyledTableCell align="left">Graduation Date</StyledTableCell>
+            <StyledTableCell align="left">Specialization</StyledTableCell>
+            <StyledTableCell align="left">Career Link</StyledTableCell>
+            <StyledTableCell align="left">Edit</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -73,12 +74,12 @@ export default function CustomizedTables({data}) {
               <StyledTableCell component="th" scope="row">
                 {row.company.companyName}
               </StyledTableCell>
-              <StyledTableCell align="right">{row.jobTitle}</StyledTableCell>
-              <StyledTableCell align="right">{formattedDate(row.jobStartDate).format1}</StyledTableCell>
-              <StyledTableCell align="right">{row.universityName}</StyledTableCell>
-              <StyledTableCell align="right">{formattedDate(row.graduationDate).format1}</StyledTableCell>
-              <StyledTableCell align="right">{row.specialization}</StyledTableCell>
-              <StyledTableCell align="right" >
+              <StyledTableCell align="left">{row.jobTitle}</StyledTableCell>
+              <StyledTableCell align="left">{formattedDate(row.jobStartDate).format1}</StyledTableCell>
+              <StyledTableCell align="left">{row.universityName}</StyledTableCell>
+              <StyledTableCell align="left">{formattedDate(row.graduationDate).format1}</StyledTableCell>
+              <StyledTableCell align="left">{row.specialization}</StyledTableCell>
+              <StyledTableCell align="left" >
                   <a href={`${row.company.careerUrl}`} target="_blank">Apply</a>
                 </StyledTableCell>
               <StyledTableCell align="right" onClick={()=>history.push(`/editrecord/${row._id}`)} >
