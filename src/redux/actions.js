b7768582a07f2ad1b12a4694ad17ee4d41d7fcd0
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-04-04 10:07:12 -0400

    fixed bugs

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,23 +1,9 @@
-// import { MOVE_UP, MOVE_DOWN } from "./actionTypes";
+import { STORE_RECORDS } from "./actionTypes";
 
 
 
-// export const moveUp = (load) => ({
-//   type: MOVE_UP,
-//   payload: load
-// });
-// // export const moveUp = (clickedName,beatRank) => ({
-// //   type: MOVE_UP,
-// //   payload: {
-// //     clickedName,
-// //     beatRank
-// //   }
-// // });
+export const storeRecords = (data) => ({
+  type: STORE_RECORDS,
+  payload: data
+});
 
-// // export const moveDown = (clickedName,beatRank) => ({
-// //   type: MOVE_DOWN,
-// //   payload: {
-// //     clickedName,
-// //     beatRank
-// //   }
-// // });
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import ranking from "./ranking";
+import records from "./records";
 
-export default combineReducers({ ranking });
+export default combineReducers({ ranking,records });
