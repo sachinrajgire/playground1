28f41b6ac3ff17a21ff0a30b9c754918eb055555
Prasoon Shakya <pras95shakya@gmail.com>
2021-08-26 18:18:11 -0700

    completed: added the pop up modal on the homescreen 	Had to make adjustment by adding multiple linebreak as the homescreen didn't have enoug content to scroll on the pade and the pop up was only supposed to be displayed after scrolling 25%

--- src/components/ScreenGrab/Screengrab.js
+++ src/components/ScreenGrab/Screengrab.js
@@ -1,27 +1,29 @@
-import "./Screengrab.css"
-import ClearIcon from '@material-ui/icons/Clear';
+import "./Screengrab.css";
+import ClearIcon from "@material-ui/icons/Clear";
 import { useState, useEffect } from "react";
 
 const Screengrab = () => {
+  const [out, setOut] = useState("");
 
-    const[out, setOut] = useState('')
-
-    const getOut = ()=>{
-        setOut('none')
-
-    }
-
-    return (
-        <div style={{display:out}} className="screen-grab">
-            <div className="cross-btn">
-            <button onClick={()=>getOut()} ><ClearIcon></ClearIcon></button>
-            </div>
-            <div className="content-screen">
-            <h3>Wynisco Bootcamp</h3>
-            <a href="https://www.wynisco.com/bootcamp.html">Check it Out</a>
-            </div>
+  const getOut = () => {
+    setOut("none");
+  };
+  console.log("SCREEN GRAB IS WORKING");
+  return (
+    <div className="outerDiv">
+      <div style={{ display: out }} className="screen-grab">
+        <div className="cross-btn">
+          <button onClick={() => getOut()}>
+            <ClearIcon></ClearIcon>
+          </button>
         </div>
-    )
-}
+        <div className="content-screen">
+          <h3>Wynisco Bootcamp</h3>
+          <a href="https://www.wynisco.com/bootcamp.html">Check it Out</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Screengrab
+export default Screengrab;
